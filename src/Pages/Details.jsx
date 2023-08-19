e51619da44f203e1d661ae6b724bf50db0200e0d
Pragati246 <pragatitomar246@gmail.com>
2023-08-19 23:27:30 +0530

    feat: new SEARCH feature update

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -1,20 +1,49 @@
 import React, {useState, useEffect} from 'react'
 import {useParams, Navigate} from 'react-router-dom'
 
+
+const url = 'https://api.themoviedb.org/3/account/51814608e31cd91cd4dc3c20394bb81c/rated/movies?language=en-US&page=1&sort_by=created_at.asc';
+const token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MTgxNDYwOGUzMWNkOTFjZDRkYzNjMjAzOTRiYjgxYyIsInN1YiI6IjY0ZGU2YzMxYTNiNWU2MDBjNWJjYWM1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.caZfb90yGvD4sbSGf5HvGytVjs543y68AUioc0PV-iM';
+
+
+
+
+
+
+
+
 const Details = () => {
     const {movieId} = useParams()
     const [movie , setMovie] = useState([]);
 
-    useEffect(() => {
-        fetch('https://api.themoviedb.org/3/movie/11?api_key=51814608e31cd91cd4dc3c20394bb81c')
-        .then(response => response.json())
-        .then(data => {
-            setMovie(data)
-        })
+    fetch(url, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'accept': 'application/json'
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data); // Use the data returned from the API
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
 
-    }, [])
+
+
+    // useEffect(() => {
+    //     fetch('https://api.themoviedb.org/3/movie/11?api_key=51814608e31cd91cd4dc3c20394bb81c')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         setMovie(data)
+    //     })
+
+    // }, [])
   return (
     <> 
+    <div>detail</div>
     {
         movie!={} && 
         <div className="movie-detail-container">
@@ -24,12 +53,12 @@ const Details = () => {
             <div className="movie-detail-body">
                 <div className="movie-detail-body-row">
                     <p><b>Movie title</b></p>
-                    <p>(Rating)</p>
+                    {/* <p>(Rating)</p> */}
                 </div>
                 <div className="movie-year-length-director-row">
-                    <p>{release_date}</p>
+                    {/* <p>{release_date}</p>
                     <p>{runtime}</p>
-                    <p>Directore</p>
+                    <p>Directore</p> */}
                 </div>
                 <div className="cast-row">
 
--- src/Pages/List.jsx
+++ src/Pages/List.jsx
@@ -1,66 +1,97 @@
-import React, {useState, useEffect} from 'react'
-import {Link} from 'react-router-dom'
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const List = () => {
-    const[movies, setMovie] = useState([]);
-    const[search, setSearch] = useState([]);
-    const handleSearch = (e) => {
-        setSearch(
-            movies.filter(
-                movie => movie.original_title.toLowerCase().includes(e.target.value.toLowerCase())
-            )
-        )
+  const [movies, setMovie] = useState([]);
+  const [searchMovie, setSearchMovie] = useState("");
+  const [currentMovie] = useState("Ram");
+
+  const handleSearch = (e) => {
+    setSearchMovie(e.target.value);
+  };
+
+  console.log("search", searchMovie);
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MTgxNDYwOGUzMWNkOTFjZDRkYzNjMjAzOTRiYjgxYyIsInN1YiI6IjY0ZGU2YzMxYTNiNWU2MDBjNWJjYWM1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.caZfb90yGvD4sbSGf5HvGytVjs543y68AUioc0PV-iM",
+      },
+    };
+
+    const fetchMovies = (query) => {
+      fetch(
+        "https://api.themoviedb.org/3/search/movie?query=" +
+          query +
+          "&include_adult=false&language=en-US&page=1",
+        options
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          let movieList = [];
+          data.results.forEach((dataItem) => {
+            movieList.push(dataItem);
+          });
+          if (movieList != 0) {
+            setMovie(movieList);
+          } else {
+            console.log("No movie found");
+          }
+        });
+    };
+
+    if (searchMovie) {
+      fetchMovies(searchMovie); 
+    } else {
+      fetchMovies(currentMovie); 
     }
+  }, [searchMovie]); // Trigger the effect when searchMovie changes
 
-    // var apikey='51814608e31cd91cd4dc3c20394bb81c';
-    // var apiToken='eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MTgxNDYwOGUzMWNkOTFjZDRkYzNjMjAzOTRiYjgxYyIsInN1YiI6IjY0ZGU2YzMxYTNiNWU2MDBjNWJjYWM1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.caZfb90yGvD4sbSGf5HvGytVjs543y68AUioc0PV-iM';
-
-    console.log('search',search)
-    useEffect(() => {
-        fetch('https://api.themoviedb.org/3/movie/11?api_key=51814608e31cd91cd4dc3c20394bb81c')
-        .then(response => response.json())
-        .then(data => {
-            let movieList = [];
-            for(let movieName in data) {
-                movieList.push({...data[movieName], movieId: movieName})
-            }
-            setMovie(movieList)
-        })
-    }, [])
   return (
     <>
       <div className="search-container">
-        <input type="text" placeholder="Search" onChange={handleSearch}></input>
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchMovie}
+          onChange={handleSearch}
+        />
       </div>
       <div className="movie-list-container">
-        {
-            movies.length > 0 && search.length ==0 &&
-            movies.map(movie => 
-                <MovieCard{...movie} key={movie.id}/>)
-        }
+        {movies.map((movie) => (
+          <MovieCard {...movie} key={movie.id} />
+        ))}
       </div>
-
     </>
+  );
+};
+
+const posterBaseUrl = "https://image.tmdb.org/t/p/original";
+
+function MovieCard({ original_title, vote_average, overview, poster_path }) {
+  return (
+    <Link to={"/details"}>
+      <div className="movie-card">
+        <div className="movie-poster">
+          <img src={posterBaseUrl + poster_path} alt="dummy" />
+        </div>
+        <div className="movie-body">
+          <>
+            {original_title.length > 6
+              ? original_title.slice(0, 25) + "..."
+              : original_title}
+          </>
+          <p>
+            {overview.length > 10 ? overview.slice(0, 25) + "..." : overview}
+          </p>
+          <p>{vote_average}</p>
+        </div>
+      </div>
+    </Link>
   );
 }
 
-//Movie Card
-function MovieCard({original_title, vote_average, yearOfRelease ,length , director, cast, overview}){
-    return(
-        <Link to={'/details'}>
-            <div className="movie-card">
-                {/* <div className="movie-img">
-                    <img src={movieImg}/>
-                </div> */}
-                <div className="movie-body">
-                    <h2>{original_title}</h2>
-                    <h3>{overview}</h3>
-                    <h3>{vote_average}</h3>
-                </div>
-            </div>
-        </Link>
-    )
-}
-
-export default List
+export default List;
